07ab7b2aa51b3942b54f4b601a1fb5c39cfefa2f
Ezex <ezequielmiranda87@gmail.com>
2018-07-10 16:08:46 -0300

    refactorizando code

---
+++ controllers/product.js
@@ -0,0 +1,78 @@
+'use strict'
+const Product = require('./models/product')
+
+function getProduct(req, res){
+    const productId = req.params.id
+    Product.findById(productId)
+    .then((product)=>{
+        res.status(200).send({product})
+    })
+    .catch((err)=>{
+         res.status(500).send({message:'Error al intentar recuperar el recurso', err: err})
+    })
+
+}
+
+function getProducts(req, res){
+    Product.find({})
+    .then((products)=>{
+        res.status(200).send({products})
+    })
+    .catch((err)=>{
+        res.status(500).send({message:'Error al intentar recuperar el recurso', err: err})
+    })
+}
+
+
+
+function updateProduct(req, res){
+    const productId = req.params.id
+    const productBody = req.body
+
+    Product.findByIdAndUpdate(productId, productBody)
+    .then((productUpdated)=>{
+        res.status(200).send({product: productUpdated})
+    })
+    .catch((err)=>{
+        res.status(500).send({message:`error al actualizar en la base de datos ${err}`})
+    })
+}
+
+function saveProduct(){
+    console.log('POST /api/product')
+    console.log(req.body)
+
+    const product = new Product()
+    product.name = req.body.name
+    product.picture = req.body.picture
+    product.price = req.body.price
+    product.category = req.body.category
+    product.description = req.body.description
+    product.save()
+    .then((productStored)=>{
+        res.status(200).send({product: productStored})
+    }).catch((err)=>{
+        res.status(500).send({message:`error al guardar en la base de datos ${err}`})
+    })
+}
+
+function deleteProduct(req, res){
+    const productId = req.params.id
+    Product.findById(productId)
+    .then((product)=>{
+        product.remove()
+    .then((product)=>{
+         res.status(200).send({message:'Producto eliminado'})
+    })
+    }).catch((err)=>{
+        res.status(500).send({message:`error al eliminar el producto ${err}`})
+    })
+}
+
+module.exports = {
+    getProduct,
+    getProducts,
+    saveProduct,
+    updateProduct,
+    deleteProduct
+}
--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 
 const express = require('express')
 const mongoose = require('mongoose')
-const Product = require('./models/product')
+const ProductCtrl = require('./controllers/product') 
 
 
 const port = process.env.PORT || 3000;  // Set the port as enviroment var
@@ -19,71 +19,13 @@ app.use(express.urlencoded())
 app.use(express.json())
 
 
-app.get("/api/product", (req, res)=>{
-    Product.find({})
-    .then((products)=>{
-        res.status(200).send({products})
-    })
-    .catch((err)=>{
-        res.status(500).send({message:'Error al intentar recuperar el recurso', err: err})
-    })
-})
+app.get("/api/product", ProductCtrl.getProducts())
+app.get('/api/product/:id', ProductCtrl.getProduct())
+app.post('/api/product', ProductCtrl.saveProduct())
+app.put('/api/product/:id', ProductCtrl.updateProduct())
+app.delete('/api/product/:id', ProductCtrl.deleteProduct())
 
-app.get('/api/product/:id', (req, res)=>{
-    const productId = req.params.id
-    Product.findById(productId)
-    .then((product)=>{
-        res.status(200).send({product})
-    })
-    .catch((err)=>{
-         res.status(500).send({message:'Error al intentar recuperar el recurso', err: err})
-    })
-})
 
-app.post('/api/product', (req, res)=>{
-    console.log('POST /api/product')
-    console.log(req.body)
-
-    const product = new Product()
-    product.name = req.body.name
-    product.picture = req.body.picture
-    product.price = req.body.price
-    product.category = req.body.category
-    product.description = req.body.description
-    product.save()
-    .then((productStored)=>{
-        res.status(200).send({product: productStored})
-    }).catch((err)=>{
-        res.status(500).send({message:`error al guardar en la base de datos ${err}`})
-    })
-})
-
-app.put('/api/product/:id', (req, res)=>{
-    const productId = req.params.id
-    const productBody = req.body
-
-    Product.findByIdAndUpdate(productId, productBody)
-    .then((productUpdated)=>{
-        res.status(200).send({product: productUpdated})
-    })
-    .catch((err)=>{
-        res.status(500).send({message:`error al actualizar en la base de datos ${err}`})
-    })
-})
-
-app.delete('/api/product/:id', (req, res)=>{
-    const productId = req.params.id
-    Product.findById(productId)
-    .then((product)=>{
-        product.remove()
-    .then((product)=>{
-         res.status(200).send({message:'Producto eliminado'})
-    })
-    }).catch((err)=>{
-        res.status(500).send({message:`error al eliminar el producto ${err}`})
-    })
-
-})
 
 mongoose.connect('mongodb://localhost:27017/shop', (err, res)=>{
     if (err) {
